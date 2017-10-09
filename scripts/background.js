d95733f685593597f1eff73bbfd9d1e4fef660c5
Deg Valentine <degvalentine@gmail.com>
2017-10-09 00:20:51 -0700

    Add boilerplate message passing.

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,27 @@
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
-});
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        chrome.pageAction.show(sender.tab.id);
+        sendResponse(settings.toObject());
+    }
+);
 
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse(settings.toObject());
-  });
+    function(request, sender, sendResponse) {
+        chrome.pageAction.show(sender.tab.id);
+
+		if (!request.command && !localStorage.getItem('trello_token')) {
+			chrome.tabs.create({url: chrome.extension.getURL('settings/index.html')});
+			sendResponse();
+			return true;
+		}
+
+
+        // Now we have a token saved locally, as fetched from the settings page after authorization.
+        if (request.command == 'saveToken') {
+            localStorage.setItem('trello_token', request.token);
+            sendResponse();
+            return true;
+        }
+
+    }
+);
--- settings/js/app.js
+++ settings/js/app.js
@@ -10,7 +10,16 @@ function init() {
                 expiration: "never",
                 interactive: false,
                 scope: {read: true, write: false},
-                success: function () {},
+                success: function () {
+                    chrome.extension.sendMessage({
+                        command: 'saveToken',
+                        token: localStorage.getItem('trello_token')
+                    }, function(data) {
+                        chrome.tabs.getCurrent(function (tab) {
+                            chrome.tabs.remove(tab.id)
+                        });
+                    });
+                },
                 error: function () {
                     alert("Failed to authorize with Trello.")
                 }
