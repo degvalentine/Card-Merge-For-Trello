29a2eb10fd40ebb47624b76c0d28b35a2a6a4115
Deg Valentine <degvalentine@gmail.com>
2017-10-14 02:08:09 -0700

    Detect card drag.

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,33 @@ chrome.extension.sendMessage({}, function(response) {
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/main.js");
 
-
+			initCardMerge();
 
 			// ----------------------------------------------------------
 
 		}
 	}, 10);
 });
+
+
+function initCardMerge()
+{
+	console.log('initCardMerge');
+	$('.list-card').attrchange({
+		//trackValues: true, 
+		callback: function (e) {
+			if (e.attributeName !== 'class') {
+				return;
+			}
+			if (e.target.classList.contains('ui-sortable-helper')) {
+				console.log('start', e);
+				$(this).data('dragging', true);
+			} else if ($(this).data('dragging')) {
+				console.log('stop', e);
+				$(this).data('dragging', false)
+			}
+
+		}
+	});
+	// use a.placeholder to show merge intent
+}
